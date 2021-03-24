c49b7a479bf44ba2968e1654f0e82fe9bb78792e
Artyom S <artemisios2014@gmail.com>
2021-03-25 01:00:22 +1000

    speed code style fix

--- js/module.js
+++ js/module.js
@@ -5,8 +5,8 @@ https://stackoverflow.com/questions/17776940/javascript-module-pattern-with-exam
 https://flaviocopes.com/javascript-iife/
 
 invoked Function Expressions (IIFE) -
-An Immediately-invoked Function Expression is 
-a way to execute functions immediately, 
+An Immediately-invoked Function Expression is
+a way to execute functions immediately,
 as soon as they are created
 
 Simple way to isolate variables declarations.
@@ -19,38 +19,34 @@ Simple way to isolate variables declarations.
 
 ;(function(param) {
 
-    
 })()
 
 IIFEs starting with a semicolon
 
-This prevents issues when blindly concatenating two JavaScript files. 
-Since JavaScript does not require semicolons, you might concatenate 
+This prevents issues when blindly concatenating two JavaScript files.
+Since JavaScript does not require semicolons, you might concatenate
 with a file with some statements in its last line that causes a syntax error.
 This problem is essentially solved with “smart” code bundlers like webpack.
 
-
 */
 
-var moduleForExample = (function() {
-    let str = 'yyy';
-    return{
-        addRight: function(text) {
-            str = str + text;
-        },
-        addLeft: function(text) {
-            str = text + str;
-        },
-        getStr: function () {
-            return str;
-        }
-    }
-    
-})();
+const moduleForExample = (function () {
+  let str = 'yyy';
+  return {
+    addRight(text) {
+      str += text;
+    },
+    addLeft(text) {
+      str = text + str;
+    },
+    getStr() {
+      return str;
+    },
+  };
+}());
 
 moduleForExample.addLeft('555');
 moduleForExample.addRight('777');
 
-let result = moduleForExample.getStr();
-console.log(result);    // 555yyy777
-
+const result = moduleForExample.getStr();
+console.log(result); // 555yyy777
--- js/true-false.js
+++ js/true-false.js
@@ -1,40 +1,23 @@
 /*
 https://frontender.info/exploring-the-abyss-of-null-and-undefined-in-javascript/
 
-В JavaScript null — это литерал и ключевое слово языка, 
-которое представляет собой отсутствие какого-либо объекта. 
-Другими словами, null указывает «в никуда». 
-В свою очередь, хоть и похожий по смыслу undefined, олицетворяет отсутствие значения как такового. 
-Оба абсолютно неизменны, не имеют свойств и методов и не способны их иметь. 
-
+В JavaScript null — это литерал и ключевое слово языка,
+которое представляет собой отсутствие какого-либо объекта.
+Другими словами, null указывает «в никуда».
+В свою очередь, хоть и похожий по смыслу undefined, олицетворяет отсутствие значения как такового.
+Оба абсолютно неизменны, не имеют свойств и методов и не способны их иметь.
 
 */
 
-
-
-var app = function () {
-
-    console.log(null == undefined); //true
-    console.log(null === undefined); // !!! false
-    console.log(null == 0); // false
-    console.log(' ');
-    console.log(false == null); //false
-    console.log(' ');
-    console.log(false == 0); // true
-    console.log(true == 1); //true
-    console.log(false == '0'); // true
-    console.log(true == '1'); //true
-
-    
- 
-
-
-}();
-
-
-
-
-
-
-
-     
+const app = (function () {
+  console.log(undefined == null); // true
+  console.log(undefined === null); // !!! false
+  console.log(null == 0); // false
+  console.log(' ');
+  console.log(false == null); // false
+  console.log(' ');
+  console.log(false == 0); // true
+  console.log(true == 1); // true
+  console.log(false == '0'); // true
+  console.log(true == '1'); // true
+}());
