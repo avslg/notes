bb57d26b24207db457f007e542c1ae2d72a0aaa2
Artyom S <artemisios2014@gmail.com>
2021-03-25 13:15:31 +1000

    add callback.js

---
+++ js/callback.js
@@ -0,0 +1,30 @@
+/* eslint-disable no-console */
+/*
+
+you can pass a reference to a function as an argument in another function
+and then to call that function back right after something has happened or some task is completed.
+
+It's called a callback.
+*/
+
+function App() {
+  this.list = [1, 2, 3, 4];
+
+  this.update = function (callback) {
+    setTimeout(
+      () => {
+        this.list.push(5, 6, 7, 8);
+        console.log('data has been updated');
+        callback();
+      }, 6000,
+    );
+  };
+
+  this.getList = function () {
+    console.log(this.list);
+    return this.list;
+  };
+}
+
+const APPL = new App();
+APPL.update(APPL.getList.bind(APPL));
