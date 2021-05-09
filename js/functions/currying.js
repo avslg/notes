434ef3e38f83f9e6eb4ddaa3b8114cdb8a737745
Artyom S <artemisios2014@gmail.com>
2021-05-09 19:51:43 +1000

    add currying.js

---
+++ js/functions/currying.js
@@ -0,0 +1,42 @@
+/*
+length property determines the number of arguments expected by the function
+
+currying (карирование)
+
+Currying is a tehnique that transforms a function
+with multiple arguments into a series of functions,
+in which each function only single argument.
+*/
+
+function multiply(a, b, c) {
+  return a * b * c;
+}
+
+/* example, manual currying function */
+let manual = function getManualCurridFumction(fname) {
+  return function putA(a) {
+    return function putB(b) {
+      return function putC(c) {
+        return fname(a, b, c);
+      };
+    };
+  };
+};
+console.log(manual(multiply)(5)(5)(5)); // 125
+
+/* example 2, implementation universal currying function */
+function getCurridFumction(fname) {
+  return function curriedFunction(...firstInvokedArgs) {
+    if (firstInvokedArgs.length >= fname.length) {
+      return fname.call(null, ...firstInvokedArgs);
+    }
+    return (...additionalArgs) => curriedFunction(...firstInvokedArgs.concat(additionalArgs));
+  };
+}
+let curriedFunction = getCurridFumction(multiply);
+
+// console.log(curriedFunction(1, 2, 3, 5)(5)); // TypeError
+console.log(curriedFunction(1, 2, 3, 5)); // 6
+console.log(curriedFunction(1, 2)(3, 5)); // 6
+console.log(curriedFunction(1)(2, 3)); // 6
+console.log(curriedFunction(1)(2)(3)); // 6, it is standard currying
